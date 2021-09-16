677d54f7801fd3803f441ea7dfba4b4dced9ad13
Steven Harder <stevenharderjr@gmail.com>
2021-09-15 18:06:04 -0600

    Refactors to try ETL on MongoDB

--- server/database/etl.js
+++ server/database/etl.js
@@ -11,21 +11,49 @@ const etlSources = {
   photos: './dataDump/reviews_photos.csv'
 };
 
-const getColNames = (sourceFile, insertCallback, exit) => {
-  const rd = readline.createInterface({
-    input: fs.createReadStream(sourceFile),
-    // output: process.stdout,
-    console: false
-  });
-
-  let rows = 0;
+const mongoETL = async (sourceFile, insertCallback, exit) => {
+  // const rd = readline.createInterface({
+  //   input: fs.createReadStream(sourceFile),
+  //   // output: process.stdout,
+  //   console: false
+  // });
+  const chunkSize = 10000000;
+  let records = 0;
   const start = Date.now();
+  let partialLine = '';
 
-  rd.on('line', line => {
-    insertCallback(rows++ ? JSON.parse('[' + line + ']') : line.split(','));
-  });
+  const stream = fs.createReadStream(sourceFile, { highWaterMark: chunkSize });
+  for await (const data in stream) {
+    const lines = data.split('\n');
+    let start = 0;
+    const end = lines.length - 1;
+    const lastLine = lines[end];
+    const lastIndex = lastLine.length - 1;
+    const lastChar = lastIndex > 0 ? lastLine[lastIndex] : '';
 
-  rd.on('close', () => {
+    if (partialLine.length) {
+      lines[0] = partialLine + lines[0];
+    }
+    partialLine = lastChar ? lastLine : '';
+
+    if (records++ === 0) {
+      insertCallback(lines[0].split(','));
+      start = 1;
+    }
+
+    for (let i = start; i < end; i++) {
+      insertCallback(JSON.parse('[' + lines[i] + ']'));
+    }
+  }
+
+  // rd.on('line', line => {
+  //   insertCallback(rows++ ? JSON.parse('[' + line + ']') : line.split(','));
+  // });
+
+  stream.on('close', () => {
+    if (partialLine.length) {
+      insertCallback(JSON.parse('[' + lines[i] + ']'));
+    }
     const time = Date.now() - start;
     const message = time > 1000 ? time + ' seconds' : time + 'ms';
     console.log(`Duration: ${message}`);
